d27a0e8f4f586027cb5c1327185b9deb0d4a47a5
kang2453 <kang2453@gmail.com>
2024-12-12 11:36:24 +0900

    fix: Simplify namespace_id retrieval with assignment expression
    Signed-off-by: kang2453 <kang2453@gmail.com>

--- src/spaceone/inventory_v2/manager/namespace_manager.py
+++ src/spaceone/inventory_v2/manager/namespace_manager.py
@@ -22,8 +22,7 @@ class NamespaceManager(BaseManager):
             _LOGGER.info(f"[ROLLBACK] Delete namespace : {vo.name} ({vo.namespace_id}) ({vo.namespace_group_id})")
             vo.delete()
         
-        namespace_id = params.get("namespace_id")
-        if namespace_id is None:
+        if namespace_id := params.get("namespace_id"):
             params["namespace_id"] = utils.generate_id("ns")
 
         namespace_vo: Namespace = self.namespace_model.create(params)
